dd4d5d06c71a61169f747ba5106cd92225c61430
Jiaming <pandasyr@users.noreply.github.com>
2020-03-12 06:29:30 -0700

    Update test.js

--- test.js
+++ test.js
@@ -20,18 +20,18 @@ function create(target, port) {
         for(var i=0; i < web_o.length; i++) {
           if(web_o[i](req, res, proxyRes, options)) { break; }
         }
-        var body = new Buffer('');
-        proxyRes.on('data', data => body = Buffer.concat([body, data]));
-        proxyRes.on('end', () => {
-            body = body.toString('UTF-8');
-            body.replace(new RegExp('http://' + target + '/', 'g'), '/');
-            body.replace(new RegExp('https://' + target + '/', 'g'), '/');
-            res.send(body);
-        })
-        //proxyRes
-           // .pipe(replace(new RegExp('http://' + target + '/', 'g'), '/'))
-           // .pipe(replace(new RegExp('https://' + target + '/', 'g'), '/'))
-           //.pipe(res);
+//        var body = new Buffer('');
+//        proxyRes.on('data', data => body = Buffer.concat([body, data]));
+//        proxyRes.on('end', () => {
+//            body = body.toString('UTF-8');
+//            body.replace(new RegExp('http://' + target + '/', 'g'), '/');
+//            body.replace(new RegExp('https://' + target + '/', 'g'), '/');
+//            res.send(body);
+//        })
+        proxyRes
+           .pipe(replace(new RegExp('http://' + target + '/', 'g'), '/'))
+           .pipe(replace(new RegExp('https://' + target + '/', 'g'), '/'))
+           .pipe(res);
     });
   });
   app.listen(port);
