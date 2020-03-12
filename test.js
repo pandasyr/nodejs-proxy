cc44cbde656bcf8500b611bc1f7d898072766bee
Jiaming <pandasyr@users.noreply.github.com>
2020-03-12 04:39:55 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,22 @@
+var http = require('http'),
+    connect = require('connect'),
+    httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer({followRedirects: true});
+
+function create(target, port) {
+  let app = connect();
+  app.use(require('../')([], selects));
+
+  app.use(function (req, res) {
+    req.headers.host = target;
+    proxy.web(req, res, { target: 'http://' + target});
+  });
+  app.use(function (req, res) {
+    console.log(JSON.stringify(res));
+  });
+  http.createServer(app).listen(port);
+}
+
+create('www.google.com', 5052);
+create('forums.huaren.us', 5053);
